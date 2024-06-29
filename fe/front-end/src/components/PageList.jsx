3ee9f072502ace203cf08165ea11e97ddf5b5939
Roger Duong <z5428838@ad.unsw.edu.au>
2024-06-30 00:43:24 +1000

    view exercise

--- fe/front-end/src/components/PageList.jsx
+++ fe/front-end/src/components/PageList.jsx
@@ -1,35 +1,37 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './HomePage/Home';
-import Login from './LoginPage/Login';
-import Register from './RegisterPage/Register';
-import Recomendation from './Recomendation';
-import ChatBox from './ChatBox';
-import Setup from './RegisterProgress/Setup';
-import SetGoal from './RegisterProgress/SetGoal';
-import SetAge from './RegisterProgress/SetAge';
-import SetupHeight from './RegisterProgress/SetupHeight';
-import SetupGender from './RegisterProgress/SetupGender';
-import DashBoard from './DashBoard';
-import SetupWeight from './RegisterProgress/SetupWeight';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./HomePage/Home";
+import Login from "./LoginPage/Login";
+import Register from "./RegisterPage/Register";
+import Recomendation from "./Recomendation";
+import ChatBox from "./ChatBox";
+import Setup from "./RegisterProgress/Setup";
+import SetGoal from "./RegisterProgress/SetGoal";
+import SetAge from "./RegisterProgress/SetAge";
+import SetupHeight from "./RegisterProgress/SetupHeight";
+import SetupGender from "./RegisterProgress/SetupGender";
+import DashBoard from "./DashBoard";
+import SetupWeight from "./RegisterProgress/SetupWeight";
+import View from "./Viewing/View";
 
 const PageList = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<DashBoard />} />
-        <Route path='/recomendation' element={<Recomendation />} />
-        <Route path='/chatbox' element={<ChatBox />} />
-        <Route path='/setup/0' element={<Setup />} />
-        <Route path='/setup/1' element={<SetGoal />} />
-        <Route path='/setup/2' element={<SetAge />} />
-        <Route path='/setup/3' element={<SetupHeight />} />
-        <Route path='/setup/4' element={<SetupGender />} />
-        <Route path='/setup/5' element={<SetupWeight />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/recomendation" element={<Recomendation />} />
+        <Route path="/chatbox" element={<ChatBox />} />
+        <Route path="/setup/0" element={<Setup />} />
+        <Route path="/setup/1" element={<SetGoal />} />
+        <Route path="/setup/2" element={<SetAge />} />
+        <Route path="/setup/3" element={<SetupHeight />} />
+        <Route path="/setup/4" element={<SetupGender />} />
+        <Route path="/setup/5" element={<SetupWeight />} />
+        <Route path="/viewing" element={<View />} />
       </Routes>
     </>
   );
