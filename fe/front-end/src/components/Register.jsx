67c18cab3d8f94c4a085bee82bbe4bb9d4c2552e
Roger Duong <z5428838@ad.unsw.edu.au>
2024-06-29 12:23:11 +1000

    register

--- fe/front-end/src/components/Register.jsx
+++ fe/front-end/src/components/Register.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
 
-const Register = () => {
-	return (
-		<div>Register</div>
-	)
+export default function Register() {
+  const [username, setUserName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [password2, setPassword2] = React.useState("");
+
+
+  function handleSubmit(e) {
+    console.log(username);
+    console.log(password);
+    console.log(password2)
+  }
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh", // Makes the form take full viewport height
+        padding: "20px",
+        borderRadius: "8px",
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Typography
+        style={{
+          marginBottom: "200px",
+        }}
+        variant="h1"
+        component="h2"
+      >
+        PLACEHOLDER
+      </Typography>
+      <TextField
+        style={{ margin: "20px" }}
+        id="username"
+        name="username"
+        label="Username"
+        variant="outlined"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <TextField
+        id="password"
+        name="password"
+        type="password"
+        label="Password"
+        variant="filled"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+	<TextField
+		style={{ margin: "20px" }}
+        id="password"
+        name="password"
+        type="password"
+        label="Confirm Password"
+        variant="filled"
+        onChange={(e) => setPassword2(e.target.value)}
+      />
+      <Button
+        style={{ margin: "20px" }}
+        variant="contained"
+        onClick={() => handleSubmit()}
+      >
+        Register
+      </Button>
+    </Box>
+  );
 }
-
-export default Register
