f9e287b029e157e4cd7d0ff294ca6114d1f70820
Louis Nguyen <ndmn004@gmail.com>
2024-06-30 00:33:57 +1000

    Fix MT

--- fe/front-end/src/components/RegisterProgress/SetupWeight.jsx
+++ fe/front-end/src/components/RegisterProgress/SetupWeight.jsx
@@ -1,28 +1,27 @@
-import WeightInput from "./WeightInput";
-import SetupProgress from "./SetupProgress";
+import WeightInput from './WeightInput';
+import SetupProgress from './SetupProgress';
 
 const SetupWeight = () => {
   return (
     <div
       style={{
-        width: "100%",
-        height: "100%",
-        position: "relative",
-        backgroundColor: "#232323",
-        overflow: "hidden",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "flex-end",
-        justifyContent: "flex-start",
-        padding: "43px 60px 80px",
-        boxSizing: "border-box",
-        gap: "100px",
-        lineHeight: "normal",
-        letterSpacing: "normal",
-      }}
-    >
-      <SetupProgress />
-      < WeightInput />
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        backgroundColor: '#232323',
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-start',
+        padding: '43px 60px 80px',
+        boxSizing: 'border-box',
+        gap: '100px',
+        lineHeight: 'normal',
+        letterSpacing: 'normal',
+      }}>
+      <SetupProgress pre_page='setup' page='setup' page_num_str='5' />
+      <WeightInput />
     </div>
   );
 };
