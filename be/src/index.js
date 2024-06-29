0c46d85b82c77a32c007432bd2e87b5ecb393794
Duc Huy Nguyen <z5511376@ad.unsw.edu.au>
2024-06-30 01:12:48 +1000

    oke

--- be/src/index.js
+++ be/src/index.js
@@ -20,3 +20,17 @@ app.use(errorHandler());
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.get('/plan', (req, res) => {
+    const healthyAndUnhealthyIngredients = [
+        "Things that you should eat: Spinach, Chicken breast, Brown rice, Rruits, Vegetables",
+        "Things that you shouldn't eat: Soda, Fried Chicken, Sausages, Ice cream, White rice"
+    ];
+    res.send(healthyAndUnhealthyIngredients);
+});
+
+res.get('/activites', (req, res) =>{
+    listActivities = [["Push ups", ["Sets: 3", "Repetition: 10-15 per set", "Estimated Time: 5 minutes"]], ["Squats", ["Sets: 3", "Repetitions: 15-20 per set", "Estimated time: 5 minutes"]]
+    ["Plank", ["Sets: 3", "Duration: 30-60 seconds per set", "Estimated time: 5 minutes"]], ["Dumbell rows", ["Sets: 3", "Repetitions: 10-12 per set", "Estimated time: 5 minutes"]]]
+    res.send(listActivities);
+});
