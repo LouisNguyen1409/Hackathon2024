7d08b88cea3b2a72dcd70fd381317dc133a517aa
Minh Truong <tuanminh1256@gmail.com>
2024-06-30 01:39:11 +1000

    working on chatbox

--- fe/front-end/src/components/ChatBox.jsx
+++ fe/front-end/src/components/ChatBox.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import ChatBoxInput from './ChatBoxInput'
+// import SetupProgress from './RegisterProgress/SetupProgress';
 const ChatBox = () => {
 	return (
-		<div>ChatBox</div>
+		<div>
+      		<ChatBoxInput />
+		</div>
+
 	)
 }
 
---
+++ fe/front-end/src/components/ChatBoxInput.jsx
@@ -0,0 +1,22 @@
+
+const ChatBoxInput = ({ className = '' }) => {
+    return (
+        <section style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            position: 'relative',
+        }}>
+            <div>
+                Hi
+            </div>
+
+            <div>
+                Hello
+            </div>
+        </section>
+    );  
+}
+
+export default ChatBoxInput;
